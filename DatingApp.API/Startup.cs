30ecde3f9a59471a2d1d9d668f2a7511f6ef33bb
Viet Phan <viet.phanho@asnet.com.vn>
2018-11-26 17:12:15 +0700

    Asp: Add cors to service and config UseCors

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -28,8 +28,8 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +45,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
